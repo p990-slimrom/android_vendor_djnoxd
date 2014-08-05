2eff4a9bf573e94db80775baa8ae73707b597a0f
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2014-08-05 07:20:01 +0200

    initial commit

---
+++ vendor_djnoxd.mk
@@ -0,0 +1,8 @@
+# Copy f50 Camera
+#PRODUCT_COPY_FILES += \
+#    vendor/djnoxd/camera/f50.apk:system/app/f50.apk
+
+# Copy PerformanceControl
+PRODUCT_COPY_FILES += \
+    vendor/djnoxd/control/PerformanceControl-2.2.4.apk:system/app/PerformanceControl-2.2.4.apk
+
