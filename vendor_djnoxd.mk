1d184b539aaf2417439644ba69c23c31affa2541
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2014-08-12 21:02:23 +0200

    Revert "Set ART as deafult runtime"
    This reverts commit dcdbd54b9c615723abd0ea1e3a46699664dc2b37.

--- vendor_djnoxd.mk
+++ vendor_djnoxd.mk
@@ -6,6 +6,3 @@
 PRODUCT_COPY_FILES += \
     vendor/djnoxd/control/PerformanceControl-2.2.4.apk:system/app/PerformanceControl-2.2.4.apk
 
-# Set ART as deafult runtime
-PRODUCT_RUNTIMES := runtime_libart_default
-PRODUCT_RUNTIMES += runtime_libdvm
