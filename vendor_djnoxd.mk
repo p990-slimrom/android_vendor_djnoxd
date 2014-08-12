e17c9d9d55607339279b22b04dc8f38d5ceb3fb0
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2014-08-12 21:02:56 +0200

    Set ART as deafult runtime

--- vendor_djnoxd.mk
+++ vendor_djnoxd.mk
@@ -6,3 +6,6 @@
 PRODUCT_COPY_FILES += \
     vendor/djnoxd/control/PerformanceControl-2.2.4.apk:system/app/PerformanceControl-2.2.4.apk
 
+PRODUCT_PROPERTY_OVERRIDES += \
+     persist.sys.dalvik.vm.lib=libart.so
+
