a75198b2d296b14107c8493904f27284e0eb5431
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2014-08-26 20:34:40 +0200

    add Xposed Framework

--- vendor_djnoxd.mk
+++ vendor_djnoxd.mk
@@ -1,3 +1,4 @@
+# Additional apps
 # Copy f50 Camera
 #PRODUCT_COPY_FILES += \
 #    vendor/djnoxd/camera/f50.apk:system/app/f50.apk
@@ -6,6 +7,12 @@
 PRODUCT_COPY_FILES += \
     vendor/djnoxd/control/PerformanceControl-2.2.4.apk:system/app/PerformanceControl-2.2.4.apk
 
+# Copy Xposed
+PRODUCT_COPY_FILES += \
+    vendor/djnoxd/Xposed/de.robv.android.xposed.installer_v33_36570c.apk:data/app/de.robv.android.xposed.installer_v33_36570c.apk
+
+
+#System settings
 PRODUCT_PROPERTY_OVERRIDES += \
      persist.sys.dalvik.vm.lib=libart.so
 
