1e33f931c12e852620968432c266d2560e3fbf7d
Randolf Kastenmaier <randolf.kastenmaier@gmail.com>
2014-11-03 11:40:03 +0100

    remove xposed, PerformanceControl, HKThemeManager
    Change-Id: I226f0783fdb28e71ae978b8c68da5ca89f12f7f5

--- vendor_djnoxd.mk
+++ vendor_djnoxd.mk
@@ -4,15 +4,15 @@
 #    vendor/djnoxd/camera/f50.apk:system/app/f50.apk
 
 # Copy PerformanceControl
-PRODUCT_COPY_FILES += \
-    vendor/djnoxd/control/PerformanceControl-2.2.5.apk:system/app/PerformanceControl-2.2.5.apk
+#PRODUCT_COPY_FILES += \
+#    vendor/djnoxd/control/PerformanceControl-2.2.5.apk:system/app/PerformanceControl-2.2.5.apk
 
 # Copy Xposed
 PRODUCT_COPY_FILES += \
-    vendor/djnoxd/Xposed/de.robv.android.xposed.installer_v33_36570c.apk:system/app/de.robv.android.xposed.installer_v33_36570c.apk
+#   vendor/djnoxd/Xposed/de.robv.android.xposed.installer_v33_36570c.apk:system/app/de.robv.android.xposed.installer_v33_36570c.apk
 # Copy XposedModules
 PRODUCT_COPY_FILES += \
-    vendor/djnoxd/XposedModules/HKThemeManagerv7.1.apk:system/app/HKThemeManagerv7.1.apk
+#    vendor/djnoxd/XposedModules/HKThemeManagerv7.1.apk:system/app/HKThemeManagerv7.1.apk
 
 ##System settings
 #PRODUCT_PROPERTY_OVERRIDES += \
